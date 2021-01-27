6b7bfd21fed7833ff7e16e32182eb8cb952482e3
Eric Mitchell <eric.mitchell@samrstudio.com>
2021-01-26 23:04:49 -0700

    Inital page with banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="banner">
+        <v-card class="d-flex justify-center mb-6" max-width="2288">
+            <v-img
+                class="white--text"
+                max-height="374"
+                max-width="2288"
+                src="../assets/header.webp"
+            >
+                <v-card-text>Leaf & Berry Studio</v-card-text>
+            </v-img>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css"></style>
